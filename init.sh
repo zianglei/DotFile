410581392991b9bdb4fd34cafece09b424040763
zianglei <zianglei@126.com>
2020-05-10 18:27:14 +0800

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
+ln -s `pwd`/vim/vimrc ~/.vimrc
+ln -s `pwd`/zsh/zshrc ~/.zshrc
+mkdir -p ~/.config/alacritty/
+ln -s `pwd`/alacritty/alacritty.yml ~/.gitconfig
