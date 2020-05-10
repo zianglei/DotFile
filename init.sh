f6a583555265fe39b6c238be07ffa3c8f4ab54ca
zianglei <zianglei@126.com>
2020-05-10 18:31:08 +0800

    add execute permission to init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ ln -s `pwd`/tmux/tmux.conf ~/.tmux.conf
 ln -s `pwd`/vim/vimrc ~/.vimrc
 ln -s `pwd`/zsh/zshrc ~/.zshrc
 mkdir -p ~/.config/alacritty/
-ln -s `pwd`/alacritty/alacritty.yml ~/.gitconfig
+ln -s `pwd`/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
