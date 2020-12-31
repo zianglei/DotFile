f658cfda74fada8f6d000fb233845d3c08dd4f74
zianglei <zianglei@126.com>
2020-12-31 06:53:23 +0000

    Execute init.sh in the bash

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # TODO: 判断主题文件夹是否存在，autosuggestions和syntax-highlighting，autojump是否安装
 
 # 判断系统类型
@@ -12,7 +13,7 @@ echo ${machine}
 if [ ${machine} == "Linux" ]; then
 	sudo apt update
 	sudo apt install python3-dev build-essential git cmake vim tmux -y
-	sudo apt install ctags global
+	sudo apt install ctags global -y
 elif [ ${machine} == "Mac" ]; then
 	brew install cmake macvim python tmux alacritty
 	# 配置alacritty
@@ -26,11 +27,10 @@ fi
 
 mkdir -p ~/.vim && git clone https://github.com/zianglei/vim-init.git ~/.vim/vim-init
 ln -f -s `pwd`/vim/vimrc ~/.vimrc
-source ~/.vimrc
 # 安装插件
 vim -E -c PlugInstall -c qall
 # 编译YouCompleteMe
-~/.vim/bundles/YouCompleteMe/install.py --all
+~/.vim/bundles/YouCompleteMe/install.py --clangd-completer
 
 #git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
