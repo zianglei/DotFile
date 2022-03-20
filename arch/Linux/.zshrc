bfb8e82735f5170b27223a3ec0f92eb5ef44612c
zianglei <zianglei@126.com>
2022-03-20 17:40:16 +0800

    Update init.sh

--- arch/Linux/.zshrc
+++
@@ -1,3 +0,0 @@
-plugins+=(git zsh-z extract)
-export http_proxy=http://127.0.0.1:1080
-export https_proxy=http://127.0.0.1:1080
--- init.sh
+++ init.sh
@@ -15,7 +15,7 @@ echo ${machine}
 # 安装依赖
 if [ ${machine} == "Linux" ]; then
 	sudo apt update
-	sudo apt install python3-dev build-essential git cmake vim tmux -y
+	sudo apt install python3-dev build-essential git cmake vim tmux python-pip -y
 	sudo apt install ctags global -y
 	sudo apt install zsh curl wget proxychains4 -y
 elif [ ${machine} == "Mac" ]; then
@@ -26,6 +26,7 @@ elif [ ${machine} == "Mac" ]; then
 fi
 
 ################ 安装 v2ray ###############
+echo "=========> Install v2ray"
 sudo dpkg -i ${v2ray_pkg_name}
 sudo rm /etc/v2ray/config.json
 pushd /etc/v2ray && sudo ln -s client.json config.json && popd
@@ -44,21 +45,28 @@ git config --global user.name zianglei
 
 ############## 安装 oh-my-zsh ##################
 echo "=========> Install oh-my-zsh"
-proxychains4 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+while ! nc -z localhost 1080; do
+    sleep 0.1
+done
+https_proxy=http://127.0.0.1:${v2ray_http_port} curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 ln -sf `pwd`/arch/${machine}/.zshrc.common $HOME/.zshrc.common
 echo "source ~/.zshrc.common" >> $HOME/.zshrc
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
 ##############
 # 配置vim
 ##############
-
+echo "========> Configure vim"
 mkdir -p ~/.vim && git clone https://github.com/zianglei/vim-init.git ~/.vim/vim-init
 ln -f -s `pwd`/vim/vimrc ~/.vimrc
 # 安装插件
 vim -E -c PlugInstall -c qall
 # 编译YouCompleteMe
-~/.vim/bundles/YouCompleteMe/install.py --clangd-completer
+if [ -d "~/.vim/bundles/YouCompleteMe" ]; then
+  pip install --user cmake
+  export PATH=$HOME/.local/bin:$PATH
+  ~/.vim/bundles/YouCompleteMe/install.py --clangd-completer
+fi
 
 #git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 #git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
