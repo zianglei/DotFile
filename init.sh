7532fd23cd53d7949d50b36491ed9e9af2286096
zianglei <zianglei@126.com>
2022-03-20 19:37:23 +0800

    Fix zsh and zsh.arch

--- init.sh
+++ init.sh
@@ -50,8 +50,8 @@ while ! nc -z localhost 1080; do
     sleep 0.1
 done
 https_proxy=http://127.0.0.1:${v2ray_http_port} curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
-ln -sf `pwd`/arch/${machine}/.zshrc.common $HOME/.zshrc.common
-echo "source ~/.zshrc.common" >> $HOME/.zshrc
+ln -sf `pwd`/.zshrc $HOME/.zshrc
+ln -sf `pwd`/arch/${machine}/.zshrc $HOME/.zshrc.arch
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 
 ##############
