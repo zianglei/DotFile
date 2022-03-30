a750b851621a80c4a086846fc3be7158495596c9
zianglei <zianglei@126.com>
2022-03-30 22:07:25 +0800

    Not install v2ray

--- init.sh
+++ init.sh
@@ -29,16 +29,16 @@ elif [ ${machine} == "Mac" ]; then
 	ln -f -s `pwd`/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 fi
 
-################ 安装 v2ray ###############
-echo "=========> Install v2ray"
-sudo dpkg -i ${v2ray_pkg_name}
-sudo rm /etc/v2ray/config.json
-pushd /etc/v2ray && sudo ln -s client.json config.json && popd
-sudo cp v2ray/client.json /etc/v2ray/config.json 
-sudo service v2ray restart
+################# 安装 v2ray ###############
+#echo "=========> Install v2ray"
+#sudo dpkg -i ${v2ray_pkg_name}
+#sudo rm /etc/v2ray/config.json
+#pushd /etc/v2ray && sudo ln -s client.json config.json && popd
+#sudo cp v2ray/client.json /etc/v2ray/config.json 
+#sudo service v2ray restart
 
 ############### 配置 proxychains ##########
-ln -sf `pwd`/proxychains4.conf /etc/proxychains4.conf
+#ln -sf `pwd`/proxychains4.conf /etc/proxychains4.conf
 
 ############### 安装 github cli ###########
 sudo apt install gh
@@ -46,18 +46,18 @@ gh auth login
 
 ############### 配置 git ##################
 echo "=========> Configure git"
-git config --global http.proxy http://127.0.0.1:${v2ray_http_port}
-git config --global https.proxy http://127.0.0.1:${v2ray_http_port}
+#git config --global http.proxy http://127.0.0.1:${v2ray_http_port}
+#git config --global https.proxy http://127.0.0.1:${v2ray_http_port}
 git config --global user.email zianglei@126.com
 git config --global user.name zianglei
 git config --global http.sslVerify false
 
 ############## 安装 oh-my-zsh ##################
-echo "=========> Install oh-my-zsh"
-while ! nc -z localhost 1080; do
-    sleep 0.1
-done
-https_proxy=http://127.0.0.1:${v2ray_http_port} curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
+#echo "=========> Install oh-my-zsh"
+#while ! nc -z localhost 1080; do
+#    sleep 0.1
+#done
+#https_proxy=http://127.0.0.1:${v2ray_http_port} curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 ln -sf `pwd`/.zshrc $HOME/.zshrc
 ln -sf `pwd`/arch/${machine}/.zshrc $HOME/.zshrc.arch
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
